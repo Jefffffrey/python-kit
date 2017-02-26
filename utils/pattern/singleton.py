05afbb234e243970afe49e42af6db63979d2309e
Jeffrey <Jeffrey.S.Teo@gmail.com>
2017-02-26 20:30:37 +0800

    feat(utils/pattern/singleton.py):add clear method

--- utils/pattern/singleton.py
+++ utils/pattern/singleton.py
@@ -9,3 +9,9 @@ class Singleton(type):
             cls._instances[cls] = super(Singleton, cls).__call__(*args,
                                                                  **kwargs)
         return cls._instances[cls]
+
+    # 单元测试的时候,使用了单例的会在各个test之间共享,
+    # 这是不期望看到的,所以需要重新刷新
+    @staticmethod
+    def clear():
+        Singleton._instances.clear()
