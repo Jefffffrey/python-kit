fe123e3ef909bc97ea445e78aaa25b4f7814761d
Jeffrey <Jeffrey.S.Teo@gmail.com>
2018-06-06 16:33:44 +0800

    update

--- kit/tests/test_os/test_path.py
+++ kit/tests/test_os/test_path.py
@@ -1,6 +1,9 @@
+import sys
 import unittest
 
 from kit.os.path import is_ignore, abspath
+
+_is_windows = sys.platform in ('win32', 'cygwin')
 
 
 class TestPath(unittest.TestCase):
@@ -11,28 +14,32 @@ class TestPath(unittest.TestCase):
         assert is_ignore('idea', 'idea/') == False
         # 完整目录名
         assert is_ignore('idea/', 'idea/') == True
-        assert is_ignore('idea/', 'idea\\') == True
-        assert is_ignore('idea\\', 'idea/') == True
-        assert is_ignore('idea\\', 'idea\\') == True
+        if _is_windows:
+            assert is_ignore('idea/', 'idea\\') == True
+            assert is_ignore('idea\\', 'idea/') == True
+            assert is_ignore('idea\\', 'idea\\') == True
         assert is_ignore('idea/', 'idea') == False
         # 使用*通配符的文件名
         assert is_ignore('b.py', '*.py') == True
-        assert is_ignore(r'E:\hello.py', '*.py') == True
-        assert is_ignore(r'E:\hello.py', '*.pyc') == False
+        if _is_windows:
+            assert is_ignore(r'E:\hello.py', '*.py') == True
+            assert is_ignore(r'E:\hello.py', '*.pyc') == False
         # 使用*通配符的目录名
         assert is_ignore('aaa/', '*/') == True
         assert is_ignore('-test/', '*-test/') == True
 
     @staticmethod
+    # https://docs.python.org/3/library/sys.html#sys.platform
+    @unittest.skipIf(not _is_windows, 'Not on Windows')
     def test_abspath():
         assert abspath('..\hello', r'E:\MyComputer\Workspace\项目', ) == \
-            r'E:\MyComputer\Workspace\hello'
+               r'E:\MyComputer\Workspace\hello'
         assert abspath('.\hello', r'E:\MyComputer\Workspace\项目') == \
-            r"E:\MyComputer\Workspace\项目\hello"
+               r"E:\MyComputer\Workspace\项目\hello"
         assert abspath('hello', r'E:\MyComputer\Workspace\项目') == \
-            r"E:\MyComputer\Workspace\项目\hello"
+               r"E:\MyComputer\Workspace\项目\hello"
         assert abspath('.', r'E:\MyComputer\Workspace\项目') == \
-            r"E:\MyComputer\Workspace\项目"
+               r"E:\MyComputer\Workspace\项目"
 
 
 if __name__ == '__main__':
